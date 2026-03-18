3358228bb2e7e965dbfe9eed36e73ec57676135f
rhea-vyte <rhealyn@vyte.dev>
2026-03-18 08:19:05 +0800

    water mark

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import RSVPModal from './components/RSVPModal'
 import DynamicTitle from './components/DynamicTitle'
 import OpeningScreen from './components/OpeningScreen'
 import Loader from './components/Loader'
-// import Watermark from './components/Watermark'
+import Watermark from './components/Watermark'
 import ScrollToTop from './components/ScrollToTop'
 import Details from './components/pages/Details'
 import Entourage from './components/pages/Entourage'
@@ -132,7 +132,7 @@ function AppContent() {
   return (
     <div className="App min-h-screen wedding-gradient">
       <DynamicTitle />
-      {/* <Watermark /> */}
+      <Watermark />
       <ScrollToTop />
       {/* Loader - shows while preloading */}
       {isLoading && (
